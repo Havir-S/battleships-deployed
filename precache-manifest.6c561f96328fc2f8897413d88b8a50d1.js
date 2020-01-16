6d89a15926d5cc274b938e5b07c9e1f4da876978
Havir-S <Havir-S@users.noreply.github.com>
2020-01-16 13:50:53 +0100

    Updates

---
+++ precache-manifest.6c561f96328fc2f8897413d88b8a50d1.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "126fefc8fe9bc02ef60c1beb6eb061a3",
+    "url": "/battleships-deployed/index.html"
+  },
+  {
+    "revision": "ba477fc1ed75240727e7",
+    "url": "/battleships-deployed/static/css/main.047e880b.chunk.css"
+  },
+  {
+    "revision": "856e5ff984a36fcfc3fa",
+    "url": "/battleships-deployed/static/js/2.fb19a0f8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/battleships-deployed/static/js/2.fb19a0f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba477fc1ed75240727e7",
+    "url": "/battleships-deployed/static/js/main.1efa742b.chunk.js"
+  },
+  {
+    "revision": "02285049bbea724f5ab2",
+    "url": "/battleships-deployed/static/js/runtime-main.855ea2e2.js"
+  },
+  {
+    "revision": "ececde2ab8be4988ea051e28dd364d6c",
+    "url": "/battleships-deployed/static/media/5-dots.ececde2a.png"
+  },
+  {
+    "revision": "95d2e27f170c95d5c9ebb881e8745228",
+    "url": "/battleships-deployed/static/media/allyNormal.95d2e27f.png"
+  },
+  {
+    "revision": "2591bd28cd551d0b2730ec5261fff3b6",
+    "url": "/battleships-deployed/static/media/allyScared.2591bd28.png"
+  },
+  {
+    "revision": "30676f5d1d0f7ef206836cabe3b26e3a",
+    "url": "/battleships-deployed/static/media/backgroundWater.30676f5d.jpg"
+  },
+  {
+    "revision": "01820855844c2ee19bd63fb423bc6a40",
+    "url": "/battleships-deployed/static/media/captain5.01820855.png"
+  },
+  {
+    "revision": "eb53902ce2eb3eba52e293bfa99ea684",
+    "url": "/battleships-deployed/static/media/captainEnemy1.eb53902c.png"
+  },
+  {
+    "revision": "9adea3be3388b2a66ffcd84f9bca1632",
+    "url": "/battleships-deployed/static/media/down-arrow.9adea3be.svg"
+  },
+  {
+    "revision": "36f8223af394761ccdb28b60207f719f",
+    "url": "/battleships-deployed/static/media/enemyNormal.36f8223a.png"
+  },
+  {
+    "revision": "695a89720fffa6e423b3f36a31a018fa",
+    "url": "/battleships-deployed/static/media/enemyScared.695a8972.png"
+  },
+  {
+    "revision": "427e08d592908493fb69272ab84bedd8",
+    "url": "/battleships-deployed/static/media/exit.427e08d5.svg"
+  },
+  {
+    "revision": "d27d156db49c2bcf066e36a781ebf64d",
+    "url": "/battleships-deployed/static/media/gear.d27d156d.svg"
+  },
+  {
+    "revision": "f0a4f4fc3dcafe8ecf286acf5958c07b",
+    "url": "/battleships-deployed/static/media/question.f0a4f4fc.svg"
+  },
+  {
+    "revision": "26779c0c0a99a00902d41a145e7d515f",
+    "url": "/battleships-deployed/static/media/rudder.26779c0c.svg"
+  },
+  {
+    "revision": "66ad3dc19697c4a38273876f6ad9b2e3",
+    "url": "/battleships-deployed/static/media/steering-wheel.66ad3dc1.svg"
+  },
+  {
+    "revision": "35c38dee8f84544e7165cf69064c956d",
+    "url": "/battleships-deployed/static/media/submarine.35c38dee.png"
+  }
+]);
